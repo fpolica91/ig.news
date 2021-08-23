954fd62c8351cce1b8aee96f50bc6756351a148f
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-08-22 22:42:28 -0400

    finishing

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,6 +2,7 @@ import styles from "./styles.module.scss"
 import { signIn, useSession } from 'next-auth/client'
 import { api } from "../../services/axios"
 import { getStripeJs } from "../../services/stripe-js"
+import { useRouter } from "next/router"
 
 interface SubscribeButtonProps {
   priceId: string
@@ -9,22 +10,24 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession()
+  const router = useRouter()
 
   async function handleSubmit() {
     if (!session) {
       signIn()
       return
-    } else {
-      try {
-        const response = await api.post('/subscribe')
-        const { sessionId } = response.data
-        const stripe = await getStripeJs()
-        await stripe.redirectToCheckout({ sessionId })
-      } catch (error) {
-        alert(error.message)
-      }
     }
-
+    if (session.activeSubscription) {
+      return router.push('/posts');
+    }
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data
+      const stripe = await getStripeJs()
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
   }
 
   return (
